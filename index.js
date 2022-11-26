7a8e7762322b52950723e664b879cee318cbf992
victor <victor.buthia@student.moringaschool.com>
2022-11-26 13:21:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+[1, 2, 3, 4, 5].filter(function (num) { return num > 3; });
+
+function findMatching(drivers, attribute) {
+  return drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+}
+
+function fuzzyMatch(drivers, attribute) {
+  return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+}
+
+function matchName(drivers, argument) {
+  return drivers.filter(function (driver) { return driver.name === argument })
+}
+
